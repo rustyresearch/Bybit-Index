c704c486f4045b7c53340b4bcd37f420f2b32246
rusty <98057610+rustyresearch@users.noreply.github.com>
2022-11-18 20:49:42 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from bybit_api import get_balances
+from constituents import calculate_weights
+from portfolio_rebalancing import rebalance
+
+balances = get_balances()
+print(f'Portfolio before rebalance: {balances}')
+weights = calculate_weights()
+print(f'Updated weights: {weights}')
+rebalance(balances, weights)
+print(f'Portfolio after rebalance: {get_balances()}')
