c1ea991e31bdce150275c249c9a4aa7e9352e4ad
rusty <98057610+rustyresearch@users.noreply.github.com>
2022-11-18 20:48:30 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+pairing = 'USDT'
+default_ratio = 0.2
+coingecko_request_interval = 0.65
+constituents_file = 'coin_ids.json'
+reselect_constituents = True
+constituents_amount = 30
+bybit_order_min = 10
+wait_seconds_between_orders = 2
